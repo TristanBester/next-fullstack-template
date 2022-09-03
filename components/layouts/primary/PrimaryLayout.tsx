6ba53f90e25c7c21c23b6399b86cc1b8c5e5d8f5
Tristan Bester <tristanbester@gmail.com>
2022-09-03 18:40:04 +0200

    fix: correct interface

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head';
 import styles from './PrimaryLayout.module.css';
 
-export interface IPrimaryLayout {
-  children: String;
-}
+export interface IPrimaryLayout {}
 
 const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
   return (
